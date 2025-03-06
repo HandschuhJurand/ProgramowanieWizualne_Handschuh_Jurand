2834ae0d2a18fd28d9f95b0a722b94d5d3763986
Jurand <handschuh.jurand@gmail.com>
2025-03-06 13:08:08 +0100

    kalkulator

--- Projekt_1/Form1.Designer.cs
+++ Projekt_1/Form1.Designer.cs
@@ -28,18 +28,252 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button_0 = new Button();
+            button_2 = new Button();
+            button_5 = new Button();
+            button4 = new Button();
+            button_3 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button_1 = new Button();
+            button_4 = new Button();
+            button10 = new Button();
+            button_przecinek = new Button();
+            button_rowna = new Button();
+            button_plus = new Button();
+            button_minus = new Button();
+            button_x = new Button();
+            button16 = new Button();
+            button_back = new Button();
+            textBox2 = new TextBox();
             SuspendLayout();
+            // 
+            // button_0
+            // 
+            button_0.Location = new Point(268, 380);
+            button_0.Name = "button_0";
+            button_0.Size = new Size(112, 34);
+            button_0.TabIndex = 0;
+            button_0.Text = "0";
+            button_0.UseVisualStyleBackColor = true;
+            button_0.Click += button1_Click;
+            // 
+            // button_2
+            // 
+            button_2.Location = new Point(268, 331);
+            button_2.Name = "button_2";
+            button_2.Size = new Size(112, 34);
+            button_2.TabIndex = 1;
+            button_2.Text = "2";
+            button_2.UseVisualStyleBackColor = true;
+            button_2.Click += button2_Click;
+            // 
+            // button_5
+            // 
+            button_5.Location = new Point(268, 281);
+            button_5.Name = "button_5";
+            button_5.Size = new Size(112, 34);
+            button_5.TabIndex = 2;
+            button_5.Text = "5";
+            button_5.UseVisualStyleBackColor = true;
+            button_5.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(268, 231);
+            button4.Name = "button4";
+            button4.Size = new Size(112, 34);
+            button4.TabIndex = 3;
+            button4.Text = "8";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button_3
+            // 
+            button_3.Location = new Point(386, 331);
+            button_3.Name = "button_3";
+            button_3.Size = new Size(112, 34);
+            button_3.TabIndex = 4;
+            button_3.Text = "3";
+            button_3.UseVisualStyleBackColor = true;
+            button_3.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(386, 281);
+            button6.Name = "button6";
+            button6.Size = new Size(112, 34);
+            button6.TabIndex = 5;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(386, 231);
+            button7.Name = "button7";
+            button7.Size = new Size(112, 34);
+            button7.TabIndex = 6;
+            button7.Text = "9";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button_1
+            // 
+            button_1.Location = new Point(150, 331);
+            button_1.Name = "button_1";
+            button_1.Size = new Size(112, 34);
+            button_1.TabIndex = 7;
+            button_1.Text = "1";
+            button_1.UseVisualStyleBackColor = true;
+            button_1.Click += button8_Click;
+            // 
+            // button_4
+            // 
+            button_4.Location = new Point(150, 281);
+            button_4.Name = "button_4";
+            button_4.Size = new Size(112, 34);
+            button_4.TabIndex = 8;
+            button_4.Text = "4";
+            button_4.UseVisualStyleBackColor = true;
+            button_4.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(150, 231);
+            button10.Name = "button10";
+            button10.Size = new Size(112, 34);
+            button10.TabIndex = 9;
+            button10.Text = "7";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button_przecinek
+            // 
+            button_przecinek.Location = new Point(684, 281);
+            button_przecinek.Name = "button_przecinek";
+            button_przecinek.Size = new Size(112, 34);
+            button_przecinek.TabIndex = 10;
+            button_przecinek.Text = ",";
+            button_przecinek.UseVisualStyleBackColor = true;
+            button_przecinek.Click += button11_Click;
+            // 
+            // button_rowna
+            // 
+            button_rowna.Location = new Point(566, 380);
+            button_rowna.Name = "button_rowna";
+            button_rowna.Size = new Size(112, 34);
+            button_rowna.TabIndex = 11;
+            button_rowna.Text = "=";
+            button_rowna.UseVisualStyleBackColor = true;
+            button_rowna.Click += button12_Click;
+            // 
+            // button_plus
+            // 
+            button_plus.Location = new Point(566, 331);
+            button_plus.Name = "button_plus";
+            button_plus.Size = new Size(112, 34);
+            button_plus.TabIndex = 12;
+            button_plus.Text = "+";
+            button_plus.UseVisualStyleBackColor = true;
+            button_plus.Click += button13_Click;
+            // 
+            // button_minus
+            // 
+            button_minus.Location = new Point(566, 281);
+            button_minus.Name = "button_minus";
+            button_minus.Size = new Size(112, 34);
+            button_minus.TabIndex = 13;
+            button_minus.Text = "-";
+            button_minus.UseVisualStyleBackColor = true;
+            button_minus.Click += button14_Click;
+            // 
+            // button_x
+            // 
+            button_x.Location = new Point(566, 231);
+            button_x.Name = "button_x";
+            button_x.Size = new Size(112, 34);
+            button_x.TabIndex = 14;
+            button_x.Text = "x";
+            button_x.UseVisualStyleBackColor = true;
+            button_x.Click += button15_Click;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(684, 231);
+            button16.Name = "button16";
+            button16.Size = new Size(112, 34);
+            button16.TabIndex = 15;
+            button16.Text = "/";
+            button16.UseVisualStyleBackColor = true;
+            button16.Click += button16_Click;
+            // 
+            // button_back
+            // 
+            button_back.Location = new Point(684, 331);
+            button_back.Name = "button_back";
+            button_back.Size = new Size(112, 34);
+            button_back.TabIndex = 16;
+            button_back.Text = "<-";
+            button_back.UseVisualStyleBackColor = true;
+            button_back.Click += button17_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(347, 101);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(256, 31);
+            textBox2.TabIndex = 18;
+            textBox2.TextChanged += textBox2_TextChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(918, 450);
+            Controls.Add(textBox2);
+            Controls.Add(button_back);
+            Controls.Add(button16);
+            Controls.Add(button_x);
+            Controls.Add(button_minus);
+            Controls.Add(button_plus);
+            Controls.Add(button_rowna);
+            Controls.Add(button_przecinek);
+            Controls.Add(button10);
+            Controls.Add(button_4);
+            Controls.Add(button_1);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button_3);
+            Controls.Add(button4);
+            Controls.Add(button_5);
+            Controls.Add(button_2);
+            Controls.Add(button_0);
             Name = "Form1";
-            Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button_0;
+        private Button button_2;
+        private Button button_5;
+        private Button button4;
+        private Button button_3;
+        private Button button6;
+        private Button button7;
+        private Button button_1;
+        private Button button_4;
+        private Button button10;
+        private Button button_przecinek;
+        private Button button_rowna;
+        private Button button_plus;
+        private Button button_minus;
+        private Button button_x;
+        private Button button16;
+        private Button button_back;
+        private TextBox textBox2;
     }
 }
--- Projekt_1/Form1.cs
+++ Projekt_1/Form1.cs
@@ -6,5 +6,170 @@ namespace Projekt_1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private double wynik = 0;
+        private string aktualny_numer = "";
+        private string operacja = "";
+        private bool czy_nowy_numer = true;
+
+        private void Dopisz_numer(string numer)
+        {
+            if (czy_nowy_numer)
+            {
+                textBox2.Text = numer;
+                czy_nowy_numer = false;
+            }
+            else
+            {
+                textBox2.Text += numer;
+            }
+            textBox2.Text = textBox2.Text;
+        }
+
+        private void Wykonaj_operacje(string op)
+        {
+            if (!czy_nowy_numer)
+            {
+                if (operacja == "")
+                {
+                    wynik = double.Parse(textBox2.Text);
+                }
+                else
+                {
+                    Oblicz_wynik();
+                }
+                operacja = op;
+                czy_nowy_numer = true;
+            }
+        }
+
+        private void Oblicz_wynik()
+        {
+            double number = double.Parse(textBox2.Text);
+            switch (operacja)
+            {
+                case "+":
+                    wynik += number;
+                    break;
+                case "-":
+                    wynik -= number;
+                    break;
+                case "x":
+                    wynik *= number;
+                    break;
+                case "/":
+                    if (number != 0)
+                        wynik /= number;
+                    else
+                        MessageBox.Show("Nie mo¿na dzieliæ przez zero!");
+                    break;
+            }
+            textBox2.Text = wynik.ToString();
+        }
+
+        // przycisk 0
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dopisz_numer("0");
+        }
+        //przycisk 3
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Dopisz_numer("3");
+        }
+        // wynik
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        //przycisk 8
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Dopisz_numer("8");
+        }
+        //przycisk backspace
+        private void button17_Click(object sender, EventArgs e)
+        {
+
+        }
+        //przycisk 1
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Dopisz_numer("1");
+        }
+        // przycisk 2
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Dopisz_numer("2");
+        }
+        // przycisk 4
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Dopisz_numer("4");
+        }
+        // przycisk 5
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Dopisz_numer("5");
+        }
+        // przycisk 6
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Dopisz_numer("6");
+        }
+        // przycisk 7
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Dopisz_numer("7");
+        }
+        // przycisk 9
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Dopisz_numer("9");
+        }
+        // przycisk x
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Wykonaj_operacje("x");
+        }
+        //przycisk /
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Wykonaj_operacje("/");
+        }
+        //przycisk -
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Wykonaj_operacje("-");
+        }
+        //przycisk ,
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Wykonaj_operacje(",");
+        }
+        //przycisk +
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Wykonaj_operacje("+");
+        }
+        //przycisk =
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (!czy_nowy_numer && operacja != "")
+            {
+                Oblicz_wynik();
+                operacja = "";
+                czy_nowy_numer = true;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
